555241cf72551517bbd9c58d131a4c708a44d2ec
mehrdad chavushi <mehrdadchavushi@gmail.com>
2023-03-08 13:30:31 +0330

    add app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+import HomePage from "./pages/HomePage.js";
+import LastVideos from "./pages/LastVideos.js";
+import LastPosts from "./pages/LastPosts.js";
+
+const navTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = () => {
+  const routes = [
+    {
+      path: "/",
+      view: HomePage,
+    },
+    {
+      path: "/last-videos",
+      view: LastVideos,
+    },
+    {
+      path: "/last-posts",
+      view: LastPosts,
+    },
+  ];
+
+  const matchRoutes = routes.map((item) => {
+    return {
+      route: item,
+      isMatch: location.pathname == item.path,
+    };
+  });
+  const match = matchRoutes.find((item) => {
+    return item.isMatch;
+  });
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    };
+  }
+  document.querySelector("#app").innerHTML = match.route.view;
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.matches("[data-linf]")) {
+      navigator(e.target.href);
+    }
+  });
+  router();
+});
